198d6d91163062e18c1cd0f4cc43a30409324cdc
dusk-beep <dusk10037@gmail.com>
2025-01-02 21:12:32 +0530

    bouncing balls

--- index.js
+++ index.js
@@ -12,7 +12,71 @@ class Ball {
     this.y = y;
     this.radius = radius;
     this.rgb = rgb;
+    this.direction = random(0, 360) * (Math.PI / 180);
+    this.speed = random(1, 5);
   }
+
+  // the point of contact move the ball to the direction of the semi
+  // circle that is poibtibg to valid how
+  // update() {
+  //   if (this.x + this.radius >= width) {
+  //     this.direction = random(90, 270) * (Math.PI / 180);
+  //   }
+  //   if (this.y + this.radius >= height) {
+  //     this.direction = random(180, 360) * (Math.PI / 180);
+  //   }
+  //   if (this.x - this.radius <= 0) {
+  //     this.direction = random(90, 270) * (Math.PI / 180);
+  //   }
+  //   if (this.y - this.radius <= 0) {
+  //     this.direction = random(0, 180) * (Math.PI / 180);
+  //   }
+  // }
+
+  update() {
+    // Right boundary
+    if (this.x + this.radius >= width) {
+      this.direction = Math.PI - this.direction;
+      this.x = width - this.radius;
+    }
+
+    // Left boundary
+    if (this.x - this.radius <= 0) {
+      this.direction = Math.PI - this.direction;
+      this.x = this.radius;
+    }
+
+    // Bottom boundary
+    if (this.y + this.radius >= height) {
+      this.direction = -this.direction;
+      this.y = height - this.radius;
+    }
+
+    // Top boundary
+    if (this.y - this.radius <= 0) {
+      this.direction = -this.direction;
+      this.y = this.radius;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = `rgb(${this.rgb.r} ${this.rgb.g} ${this.rgb.b} )`;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    ctx.fill();
+  }
+
+  animate() {
+    this.x += Math.cos(this.direction) * this.speed;
+    this.y += Math.sin(this.direction) * this.speed;
+    this.update();
+  }
+
+  collission() {}
+}
+
+function random(low, upper) {
+  return Math.floor(Math.random() * (upper - low + 1) + low);
 }
 
 function getRandomPos() {
@@ -21,15 +85,6 @@ function getRandomPos() {
 
   console.log(x, y);
   return [x, y];
-}
-
-function renderBalls(balls) {
-  for (const ball of balls) {
-    ctx.fillStyle = `rgb(${ball.rgb.r} ${ball.rgb.g} ${ball.rgb.b} )`;
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI, false);
-    ctx.fill();
-  }
 }
 
 function getRandomRGB() {
@@ -50,5 +105,17 @@ function getRandomBalls() {
   }
 }
 
+function gameLoop() {
+  ctx.fillStyle = 'rgba(0,0,0,0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.animate();
+  }
+
+  requestAnimationFrame(gameLoop);
+}
+
 getRandomBalls();
-renderBalls(balls);
+gameLoop();
